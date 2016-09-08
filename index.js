1bd2a8b0dece2e9832a02fea5c3f7f5332cd43de
zVictor <victor0110@msn.com>
2016-09-08 13:30:49 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import Manager from './Manager';
+import Model from './Model';
+import allow from './allow';
+import injectConnectors from './injectConnectors';
+import generateResolvers from './generateResolvers';
+
+export {
+  Manager,
+  Model,
+  allow,
+  injectConnectors,
+  generateResolvers,
+};
